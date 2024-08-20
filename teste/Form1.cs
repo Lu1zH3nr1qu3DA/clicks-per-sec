4fbdfa112e45a77ea4744963ac484c6e0010dd8c
Luiz Henrique Dainez Alecrim <luiz.hdalecrim@outlook.com>
2024-08-20 13:04:56 -0300

    Adicionar arquivos de projeto.

---
+++ teste/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace teste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int clicks = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clicks++;
+            label1.Text = clicks.ToString();
+            do
+            {
+                Timer();
+            }
+            while (true);
+        }
+        private void Timer()
+        {
+            System.Timers.Timer timer = new System.Timers.Timer();
+
+            timer.Enabled = true;
+            
+        }
+    }
+}
